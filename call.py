058b5144064fd052e58d67f0070dc47677956624
Dharv <dharv.patel@gmail.com>
2023-08-10 11:10:09 -0700

    idk

--- call.py
+++ call.py
@@ -4,15 +4,15 @@ import yfinance as yf
 import tensorflow as tf
 import numpy as np
 from keras.optimizers import SGD, RMSprop, Adam
+import pandas as pd
 
-ticker = 'AAPL'
-data = yf.download(ticker, start='1980-01-01', end='2023-01-01',interval='1d')
+data = pd.read_csv('data.csv')
 
-optimizer = SGD(learning_rate=0.99, momentum=0.99)
-num_features = 3 #RSI, Moving average slope, Signal, MACD, Volume
+optimizer = RMSprop(learning_rate=20, momentum=0.9)
+num_features = 5 #RSI, Moving average slope, Signal, MACD, Volume
 num_actions = 1 #How many outputs do we want
-history_depth = 10 # num of interval points which are accessible to the model before right now
-loss_function = 'mean_squared_logarithmic_error'
+history_depth = 50 # num of interval points which are accessible to the model before right now
+loss_function = 'mean_squared_error'
 validation_set_number = 20
 
 data1 = pre_process.pre_process(data,Moving_window=14)
@@ -26,8 +26,7 @@ percentchange = data1['Change'].to_numpy()
 MACD_strength = data1['Signal_MACD_strength'].to_numpy()
 percentchange = percentchange[1:]
 percentchange = np.append(percentchange,np.NaN)
-
-data_to_input = np.column_stack((RSI,Volume,MACD_strength,percentchange))
+data_to_input = np.column_stack((RSI,Volume,MACD_strength,MACD,signal,percentchange))
 
 training_data = data_to_input[:-validation_set_number*history_depth]
 validation_data = data_to_input[-validation_set_number*history_depth:]
@@ -45,14 +44,14 @@ for i in range(len(y_data_train)):
     if (y_data_train[i,history_depth-1] > 0):
         training_labels.append(1)
     else:
-        training_labels.append(0)
+        training_labels.append(-1)
 
 testing_labels = []
 for i in range(len(y_data_val)):
     if (y_data_val[i,history_depth-1] > 0):
         testing_labels.append(1)
     else:
-        testing_labels.append(0)
+        testing_labels.append(-1)
 
 y_data_train = np.array(training_labels)
 y_data_val = np.array(testing_labels)
--- model.py
+++ model.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 
 def model(num_features, num_actions, history_depth, loss_function, optimizer):
     model = tf.keras.Sequential([ #Will get more complicated as we learn more, but a good starting point
-        #tf.keras.layers.Dense(16384, activation='tanh', input_shape=(history_depth,num_features)),
         tf.keras.layers.Dense(2048, activation='tanh', input_shape=(history_depth,num_features)),
-        ##tf.keras.layers.Dense(4096, activation='tanh'),
+        #tf.keras.layers.Dense(8192, activation='sigmoid'),
+        #tf.keras.layers.Dense(4096, activation='tanh'),
         #tf.keras.layers.Dense(2048, activation='sigmoid'),
         tf.keras.layers.Dense(1024, activation='sigmoid'),
         tf.keras.layers.Dense(256, activation='tanh'),
         tf.keras.layers.Dense(128, activation='sigmoid'),
         tf.keras.layers.Dense(16, activation='tanh'),
-        tf.keras.layers.Dense(num_actions, activation='tanh')
+        tf.keras.layers.Dense(num_actions, activation='sigmoid')
     ])
     model.compile(loss=loss_function,optimizer = optimizer, metrics=['accuracy'])
     model.summary()
